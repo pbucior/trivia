474db848945392056dd64f1ddafc412d7e1cc716
Paweł Bucior <pawel@bucior.eu>
2020-02-19 14:02:13 +0100

    Check answers

--- app/src/main/java/eu/bucior/trivia/MainActivity.java
+++ app/src/main/java/eu/bucior/trivia/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -55,8 +56,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.true_button:
+                checkAnswer(true);
                 break;
             case R.id.false_button:
+                checkAnswer(false);
                 break;
             case R.id.prev_button:
                 if (currentQuestionIndex > 0) {
@@ -69,6 +72,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 updateQuestion();
                 break;
         }
+    }
+
+    private void checkAnswer(boolean userChooseCorrect) {
+        boolean answerIsTrue = questionList.get(currentQuestionIndex).isAnswerTrue();
+        int toastMessageId = 0;
+        if (userChooseCorrect == answerIsTrue) {
+            toastMessageId = R.string.correct_answer;
+        } else {
+            toastMessageId = R.string.wrong_answer;
+        }
+        Toast.makeText(MainActivity.this, toastMessageId, Toast.LENGTH_SHORT).show();
     }
 
     private void updateQuestion() {
